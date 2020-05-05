110e37f48140952b72b842668f90d40c98030f8d
gslg-1 <61031185+gslg-1@users.noreply.github.com>
2020-05-05 14:20:07 +0200

    Fix: Added the missing cpaChecker and Unit test framework

--- exec/scripts/cpa-auto.sh
+++ exec/scripts/cpa-auto.sh
@@ -2,9 +2,13 @@
 #!!!!!!!!!!!!!!!!!!!!! Don't ferget to set CRLF to LF !!!!!!!!!!!!!!!!!!!!!
 echo "Start shell script "$0
 args="'$*'"
-for module in "$args"
-./exec/tools/cpachecker/scripts/cpa.sh -valueAnalysis-NoCegar-join -outputpath ./output/$module/CPAchecker -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL ./output/prebuild/$module.p.c
-#./exec/tools/cpachecker/scripts/cpa.sh -preprocess -valueAnalysis-NoCegar-join -outputpath ./output/CPAchecker -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL ./exec/tools/cpachecker/doc/examples/example.c
-
+rm -fr output/CPAchecker
+mkdir output/CPAchecker
+for module in "${@}"
+do
+    mkdir ./output/CPAchecker/$module
+    ./exec/tools/cpachecker/scripts/cpa.sh -valueAnalysis-NoCegar-join -outputpath ./output/CPAchecker/$module -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL ./output/prebuild/$module.p.c
+    #./exec/tools/cpachecker/scripts/cpa.sh -preprocess -valueAnalysis-NoCegar-join -outputpath ./output/CPAchecker -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL ./exec/tools/cpachecker/doc/examples/example.c
+done
 echo 'Finished shell script' $0 '.'
 #EOF
--- exec/scripts/cpa-check.sh
+++ exec/scripts/cpa-check.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #!!!!!!!!!!!!!!!!!!!!! Don't ferget to set CRLF to LF !!!!!!!!!!!!!!!!!!!!!
 echo "Start shell script "$0
-
-./exec/tools/cpachecker/scripts/cpa.sh $@ -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL
+./exec/tools/cpachecker/scripts/cpa.sh $@ -outputpath ./output/CPAchecker/$module -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL
 
 echo 'Finished shell script' $0 '.'
 #EOF
