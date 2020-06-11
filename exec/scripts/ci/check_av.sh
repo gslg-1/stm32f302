ea885699318499570db7242bcf1e0e0657d50bd0
gslg-1 <61031185+gslg-1@users.noreply.github.com>
2020-06-11 14:59:45 +0200

    Module: main Note: All Scripts completed

--- exec/scripts/ci/check_av.sh
+++ exec/scripts/ci/check_av.sh
@@ -2,5 +2,46 @@
 #!!!!!!!!!!!!!!!!!!!!! Don't ferget to set CRLF to LF !!!!!!!!!!!!!!!!!!!!!
 echo 'Start shell script '$0
 
-echo 'Finished shell script' $0 '.'
+function getRequiredModules()
+{
+    # get branchname
+    cat $1
+}
+
+function checkAvailabilityViaCommits()
+{
+    local IS_PART_OF=0
+    local RESULT='FAILURE'
+    local COMMIT_HISTORY=$(git log --pretty=format:"%h %s")
+    local MOD_LIST=$(echo $COMMIT_HISTORY | grep Module: )
+    for ELEMENT_M in "$@"
+    do
+        for ELEMENT_N in "${MOD_LIST[@]}"
+        do
+            if [ $ELEMENT_N -eq $ELEMENT_M  ]
+            then 
+                IS_PART_OF=1
+                break
+            fi
+        done
+        if [$IS_PART_OF -eq 0 ]
+        then
+            echo "Test of $ELEMENT_N not available"
+            exit 1
+        else
+            IS_PART_OF=0
+        fi
+    done 
+    echo "All Module Tests are available"
+    exit 0
+}
+
+BRANCH_NAME=$(git branch --show-current)
+# Get List of Requirde OffTarget Tests
+OFF_TAR_LIST=$(getRequiredModules /docs/dsgn/ci_specs/$BRANCH_NAME.spec)
+# Get List of Requirde OnTarget Tests
+ON_TAR_LIST=$(getRequiredModules /docs/dsgn/ci_specs/onTargetMTests/$BRANCH_NAME.spec)
+
+# Check for availability of Tests
+checkAvailability $OFF_TAR_LIST && checkAvailability  $ON_TAR_LIST && exit 0 || exit 1
 #EOF
--- exec/scripts/ci/staticAnalysis.sh
+++ exec/scripts/ci/staticAnalysis.sh
@@ -2,14 +2,37 @@
 #!!!!!!!!!!!!!!!!!!!!! Don't ferget to set CRLF to LF !!!!!!!!!!!!!!!!!!!!!
 echo 'Start shell script '$0
 
-args="'$*'"
-rm -fr output/CPAchecker
-mkdir output/CPAchecker
-for module in "${@}"
-do
-    mkdir ./output/CPAchecker/$module
-    ./exec/tools/cpachecker/scripts/cpa.sh -default -outputpath ./output/CPAchecker/$module -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL ./output/prebuild/$module.p.c
-done
+function getRequiredModules()
+{
+    # get branchname
+    BRANCH_NAME=$(git branch --show-current)
+    cat docs/dsgn/$BRANCH_NAME.spec
+}
+
+function clearWDir()
+{
+    rm -fr $1 
+    mkdir  $1
+
+}
+function analyseAll()
+{
+    for module in "${@}"
+    do
+        mkdir ./output/CPAchecker/$module
+        ./exec/tools/cpachecker/scripts/cpa.sh -default -outputpath ./output/CPAchecker/$module -setprop analysis.checkCounterexamples=false -setprop solver.solver=SMTInterpol -setprop cpa.predicate.encodeBitvectorAs=INTEGER -setprop cpa.predicate.encodeFloatAs=RATIONAL ./output/prebuild/$module.p.c
+    done
+}
+
+
+# Get List of Required Modules 
+MODULE_LIST=$(getRequiredModules)
+
+# Clear Working Directory
+clearWDir output/CPAchecker
+
+# Check Every Module
+analyseAll $MODULE_LIST
 
 
 echo 'Finished shell script' $0 '.'
